9074aa6856b5488a5baedf6194e20c59246a7c9f
İsmet İnan <ismet11inan@gmail.com>
2025-07-19 05:03:45 +0300

    Origin V3

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/ismetinan/BilkentForum/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type LoginRequest struct {
@@ -10,15 +16,28 @@ type LoginRequest struct {
 	Password string `json:"password"`
 }
 type apiConfig struct {
-	Port string
+	DatabaseQueries *database.Queries
 }
 
 func main() {
 	// This is the main entry point of the application.
 	// You can initialize your application here.
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
 
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		return
+	}
+	defer db.Close()
+	apiConfig := &apiConfig{
+		DatabaseQueries: database.New(db),
+	}
+	fmt.Println("Database connection established")
+	fmt.Println("Starting the Bilkent Forum API...")
+	fmt.Println(apiConfig.DatabaseQueries)
 	mux := http.NewServeMux()
-
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.23.0
 
 toolchain go1.23.11
 
-require golang.org/x/crypto v0.40.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+)
